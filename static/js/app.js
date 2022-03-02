ce8215087272ad58ceef7812534ddcbe43b5a481
Kaiya Hull <kaiyahull@outlook.com>
2022-03-01 22:25:35 -0600

    Adding completed app.js file for module

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,29 @@ function buildTable(data) {
  });
 
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = h3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Check to see if a date was entered and filter the data use that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the
+        // `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+
+    };
+
+    // Rebuild the table using the filtered data
+    // @Note: If no date was entered the filteredData will be the original
+    // tableData
+    buildTable(filteredData);
+
+};
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
